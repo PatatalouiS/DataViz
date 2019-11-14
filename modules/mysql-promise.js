18ac364b94293ef99027a9b9bde4bc332425b1b8
Maxime OLIVIE <maxime.olivie@etu.univ-lyon1.fr>
2019-11-14 10:36:22 +0100

    Modification du module mysql-promise :
    - utilisation des promise plus simple
    - Indentation syntaxique

--- modules/mysql-promise.js
+++ modules/mysql-promise.js
@@ -3,37 +3,28 @@
 
 const mysql = require('mysql');
 
-
 // ------------------------- Création et connection SGBD ------------------------- //
 
-
-const create = settings =>
-{
+const create = settings => {
 	const SQLConnection = mysql.createConnection(settings);
 
-	const SQLConnect = () => new Promise (resolve =>
-	{
-		SQLConnection.connect(err =>
-		              {
-		              	if(err) throw err;
-		              	resolve;
-		              });
+	const SQLConnect = () => new Promise (resolve => {
+		SQLConnection.connect(err => {
+			if(err) throw err;
+			resolve;
+		});
 	});
 
-	const SQLQuery = (query, values) => new Promise (resolve =>
-	{
-			SQLConnection.query(query, values, (err, res) =>
-		                 {
-		                 	if(err) throw err;
-		                 	resolve(res);
-		                 });
+	const SQLQuery = (query, values) => new Promise (resolve => {
+		SQLConnection.query(query, values, (err, res) => {
+			if(err) throw err;
+			resolve(res);
+		});
 	});
 
 	return SQLQuery
 };
 
-
 // -------------------------------- Requêtes ------------------------------------ //
-
 
 module.exports = create;
